ef1030ad75c06fa4bbcdb601568b4a2d1aafb56f
Sabari <ssgreeshan@gmail.com>
2026-04-04 18:12:14 +0530

    well its okay

--- src/app/pages/page1/page1/page1.ts
+++ src/app/pages/page1/page1/page1.ts
@@ -1,9 +1,148 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Api } from '../../../shared/services/api';
 
 @Component({
   selector: 'app-page1',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './page1.html',
   styleUrl: './page1.scss',
 })
-export class Page1 {}
+export class Page1 {
+  private fb = inject(FormBuilder);
+  // private srv = inject(Api);
+
+  public pageForm = this.fb.group({});
+  public tableRows = [
+    {
+      displayName: 'test 1',
+      type: 'main',
+      elements: [
+        {
+          type: 'form',
+          fields: [
+            {
+              label: 'Text Data',
+              key: 'textData',
+            },
+            {
+              label: 'Number Data',
+              key: 'numData',
+            },
+          ],
+        },
+        {
+          type: 'cards',
+          fields: [
+            {
+              label: 'Total',
+              key: 'total',
+              actions: [
+                {
+                  act: 'click',
+                },
+                {
+                  act: 'drag',
+                },
+              ],
+            },
+            {
+              label: 'Today',
+              key: 'today',
+              actions: [
+                {
+                  act: 'click',
+                },
+                {
+                  act: 'drag',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: 'table',
+          fields: [
+            {
+              label: 'S.No',
+              key: '$index',
+            },
+            {
+              label: 'Text Data',
+              key: 'textData',
+            },
+            {
+              label: 'Number Data',
+              key: 'numData',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      displayName: 'test 2',
+      type: 'main',
+      elements: [
+        {
+          type: 'form',
+          fields: [
+            {
+              label: 'Text Data',
+              key: 'textData',
+            },
+            {
+              label: 'Number Data',
+              key: 'numData',
+            },
+          ],
+        },
+        {
+          type: 'cards',
+          fields: [
+            {
+              label: 'Total',
+              key: 'total',
+              actions: [
+                {
+                  act: 'click',
+                },
+                {
+                  act: 'drag',
+                },
+              ],
+            },
+            {
+              label: 'Today',
+              key: 'today',
+              actions: [
+                {
+                  act: 'click',
+                },
+                {
+                  act: 'drag',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: 'table',
+          fields: [
+            {
+              label: 'S.No',
+              key: '$index',
+            },
+            {
+              label: 'Text Data',
+              key: 'textData',
+            },
+            {
+              label: 'Number Data',
+              key: 'numData',
+            },
+          ],
+        },
+      ],
+    },
+  ];
+}
