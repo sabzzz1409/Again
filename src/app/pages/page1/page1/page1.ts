197d7139f694259368c75800b54b398e6b76a665
Sabari <ssgreeshan@gmail.com>
2026-04-06 12:14:30 +0530

    got bored

--- src/app/pages/page1/page1/page1.ts
+++ src/app/pages/page1/page1/page1.ts
@@ -1,16 +1,15 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { Api } from '../../../shared/services/api';
-import { JsonPipe } from '@angular/common';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-page1',
-  imports: [ReactiveFormsModule, JsonPipe],
+  imports: [FormsModule],
   templateUrl: './page1.html',
   styleUrl: './page1.scss',
 })
 export class Page1 {
-  public pageForm!: FormGroup;
+  @ViewChildren('editInput') inputs!: QueryList<ElementRef>;
+  // 5 TIER DESIGN
 
   public tableRows: any = [
     {
@@ -26,10 +25,12 @@ export class Page1 {
                 {
                   label: 'Text Data',
                   key: 'textData',
+                  actions: [{}],
                 },
                 {
                   label: 'Number Data',
                   key: 'numData',
+                  actions: [{}],
                 },
               ],
             },
@@ -68,14 +69,17 @@ export class Page1 {
                 {
                   label: 'S.No',
                   key: '$index',
+                  actions: [{}],
                 },
                 {
                   label: 'Text Data',
                   key: 'textData',
+                  actions: [{}],
                 },
                 {
                   label: 'Number Data',
                   key: 'numData',
+                  actions: [{}],
                 },
               ],
             },
@@ -96,10 +100,12 @@ export class Page1 {
                 {
                   label: 'Text Data',
                   key: 'textData',
+                  actions: [{}],
                 },
                 {
                   label: 'Number Data',
                   key: 'numData',
+                  actions: [{}],
                 },
               ],
             },
@@ -138,14 +144,17 @@ export class Page1 {
                 {
                   label: 'S.No',
                   key: '$index',
+                  actions: [{}],
                 },
                 {
                   label: 'Text Data',
                   key: 'textData',
+                  actions: [{}],
                 },
                 {
                   label: 'Number Data',
                   key: 'numData',
+                  actions: [{}],
                 },
               ],
             },
@@ -160,10 +169,12 @@ export class Page1 {
                 {
                   label: 'Text Data',
                   key: 'textData',
+                  actions: [{}],
                 },
                 {
                   label: 'Number Data',
                   key: 'numData',
+                  actions: [{}],
                 },
               ],
             },
@@ -202,14 +213,17 @@ export class Page1 {
                 {
                   label: 'S.No',
                   key: '$index',
+                  actions: [{}],
                 },
                 {
                   label: 'Text Data',
                   key: 'textData',
+                  actions: [{}],
                 },
                 {
                   label: 'Number Data',
                   key: 'numData',
+                  actions: [{}],
                 },
               ],
             },
@@ -219,21 +233,226 @@ export class Page1 {
     },
   ];
 
-  constructor(private fb: FormBuilder) {
-    this.pageForm = this.fb.group({});
+  private selectedData: any;
+  public editData: any;
+  private prevKey: any;
 
+  constructor() {
+    this.setTable();
+  }
+
+  private setTable() {
     this.tableRows.forEach((piece: any) => {
       piece.submodules.forEach((part: any) => {
         part.elements.forEach((pack: any) => {
           pack.fields.forEach((parcel: any) => {
+            parcel?.actions?.forEach((perk: any) => {
+              perk.edit = false;
+            });
             parcel.rowspan = parcel?.actions?.length || 1;
+            parcel.edit = false;
           });
           pack.rowspan = pack.fields.reduce((acc: any, item: any) => acc + item.rowspan, 0);
+          pack.edit = false;
         });
         part.rowspan = part.elements.reduce((acc: any, item: any) => acc + item.rowspan, 0);
+        part.edit = false;
       });
       piece.rowspan = piece.submodules.reduce((acc: any, item: any) => acc + item.rowspan, 0);
-      console.log(piece);
+      piece.edit = false;
     });
+  }
+
+  editAction(i = -1, j = -1, k = -1, l = -1, m = -1) {
+    let key;
+
+    this.setTable();
+
+    console.log(i, j, k, l, m);
+    const module = i > -1 ? this.tableRows[i] : null;
+    const submodule = j > -1 ? module.submodules[j] : null;
+    const element = k > -1 ? submodule.elements[k] : null;
+    const field = l > -1 ? element.fields[l] : null;
+    const action = m > -1 ? field.actions[m] : null;
+
+    if (action) {
+      action.edit = !action.edit;
+      this.selectedData = action;
+      this.editData = action?.act;
+      key = 'act';
+    } else {
+      if (field) {
+        field.edit = !field.edit;
+        this.selectedData = field;
+        this.editData = field.label;
+        key = 'label';
+      } else {
+        if (element) {
+          element.edit = !element.edit;
+          this.selectedData = element;
+          this.editData = element.type;
+          key = 'type';
+        } else {
+          if (submodule) {
+            submodule.edit = !submodule.edit;
+            this.selectedData = submodule;
+            this.editData = submodule.displayName;
+            key = 'displayName';
+          } else {
+            if (module) {
+              module.edit = !module.edit;
+              this.selectedData = module;
+              this.editData = module.displayName;
+              key = 'displayName';
+            } else {
+              this.setTable();
+              this.selectedData[this.prevKey] = this.editData;
+              this.selectedData = null;
+              this.editData = null;
+              key = null;
+            }
+          }
+        }
+      }
+    }
+
+    setTimeout(() => {
+      this.inputs?.last?.nativeElement?.focus();
+    });
+
+    this.prevKey = key;
+    console.log(this.selectedData);
+  }
+
+  addItem(i = -1, j = -1, k = -1, l = -1, m = -1) {
+    let key;
+
+    this.setTable();
+
+    console.log(i, j, k, l, m);
+    const module = i > -1 ? this.tableRows[i] : null;
+    const submodule = j > -1 ? module.submodules[j] : null;
+    const element = k > -1 ? submodule.elements[k] : null;
+    const field = l > -1 ? element.fields[l] : null;
+    const action = m > -1 ? field.actions[m] : null;
+
+    if (action) {
+      field.actions.push({
+        act: 't',
+        edit: true,
+      });
+      this.selectedData = action;
+      this.editData = action?.act;
+      key = 'act';
+    } else {
+      if (field) {
+        element.fields.push({
+          label: '',
+          edit: true,
+          actions: [{}],
+        });
+        this.selectedData = field;
+        this.editData = field.label;
+        key = 'label';
+      } else {
+        if (element) {
+          submodule.elements.push({
+            edit: true,
+            type: '',
+            elements: [
+              {
+                type: '',
+                fields: [
+                  {
+                    type: '',
+                    actions: [{}],
+                  },
+                ],
+              },
+            ],
+          });
+          this.selectedData = element;
+          this.editData = element.type;
+          key = 'type';
+        } else {
+          if (submodule) {
+            module.submodules.push({
+              edit: true,
+              displayName: '',
+              elements: [
+                {
+                  type: '',
+                  elements: [
+                    {
+                      type: '',
+                      fields: [
+                        {
+                          type: '',
+                          actions: [{}],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            });
+            this.selectedData = submodule;
+            this.editData = submodule.displayName;
+            key = 'displayName';
+          } else {
+            if (module) {
+              this.tableRows.push({
+                displayName: '',
+                edit: true,
+                submodules: [
+                  {
+                    displayName: '',
+                    elements: [
+                      {
+                        type: '',
+                        elements: [
+                          {
+                            type: '',
+                            fields: [
+                              {
+                                type: '',
+                                actions: [{}],
+                              },
+                            ],
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                ],
+              });
+              this.selectedData = module;
+              this.editData = module.displayName;
+              key = 'displayName';
+            } else {
+              this.setTable();
+              this.selectedData[this.prevKey] = this.editData;
+              this.selectedData = null;
+              this.editData = null;
+              key = null;
+            }
+          }
+        }
+      }
+    }
+
+    setTimeout(() => {
+      this.inputs?.last?.nativeElement?.focus();
+    });
+
+    this.prevKey = key;
+    this.setTable();
+    console.log(this.tableRows);
+  }
+
+  keyHandle(ev: any) {
+    if (ev.key == 'Enter') {
+      this.editAction();
+    }
   }
 }
